262dcda260de48debf88a8ad3048fa0f7aa8b344
ernie <monikachomiak@gmail.com>
2022-04-08 21:19:05 -0500

    adding player health

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,6 +30,33 @@ console.log(
   enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " remaining. "
 );
 
+// put new code under this
+console.log(
+  playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
+);
+
+// check enemy's health
+if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died! ");
+}
+
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left. ")
+}
+
+// put new code under this
+console.log(enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " remaining. " );
+
+// check player's health
+if (playerHealth <=0) {
+  window.alert(playerName + " has died. ");
+}
+
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left. ");
+}
+
+
 };
 
 // execute function
